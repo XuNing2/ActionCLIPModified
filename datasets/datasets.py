429820cfb6891e3d12d4dde9d6d1149e64394bf8
xuning <xuning446@gmail.com>
2022-05-07 13:08:02 +0000

    删除冗余代码

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -7,57 +7,9 @@ import os
 import os.path
 import numpy as np
 from numpy.random import randint
-import pdb
-import io
-import time
 import pandas as pd
-import torchvision
-import random
 from PIL import Image, ImageOps
-import cv2
-import numbers
-import math
-import torch
-from RandAugment import RandAugment
 
-class GroupTransform(object):
-    def __init__(self, transform):
-        self.worker = transform
-
-    def __call__(self, img_group):
-        return [self.worker(img) for img in img_group]
-    
-class ToTorchFormatTensor(object):
-    """ Converts a PIL.Image (RGB) or numpy.ndarray (H x W x C) in the range [0, 255]
-    to a torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0] """
-    def __init__(self, div=True):
-        self.div = div
-
-    def __call__(self, pic):
-        if isinstance(pic, np.ndarray):
-            img = torch.from_numpy(pic).permute(2, 0, 1).contiguous()
-        else:
-            img = torch.ByteTensor(torch.ByteStorage.from_buffer(pic.tobytes()))
-            img = img.view(pic.size[1], pic.size[0], len(pic.mode))
-            img = img.transpose(0, 1).transpose(0, 2).contiguous()
-        return img.float().div(255) if self.div else img.float()
-
-class Stack(object):
-
-    def __init__(self, roll=False):
-        self.roll = roll
-
-    def __call__(self, img_group):
-        if img_group[0].mode == 'L':
-            return np.concatenate([np.expand_dims(x, 2) for x in img_group], axis=2)
-        elif img_group[0].mode == 'RGB':
-            if self.roll:
-                print(len(img_group))
-                return np.concatenate([np.array(x)[:, :, ::-1] for x in img_group], axis=2)
-            else:
-                print(len(img_group))
-                rst = np.concatenate(img_group, axis=2)
-                return rst
 
     
 class VideoRecord(object):
