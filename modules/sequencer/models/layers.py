9a97a582616e43ad308d3d4fd3a988c2e30132f4
xuning <xuning446@gmail.com>
2022-05-12 11:39:34 +0000

    师兄帮助修改

--- modules/sequencer/models/layers.py
+++ modules/sequencer/models/layers.py
@@ -52,13 +52,16 @@ class GRU(RNNBase):
                           bias=bias, bidirectional=bidirectional)
 
 
-class LSTM(RNNBase):
+class LSTM(nn.Module):
 
     def __init__(self, input_size, hidden_size=None,
                  num_layers: int = 1, bias: bool = True, bidirectional: bool = True):
-        super().__init__(input_size, hidden_size, num_layers, bias, bidirectional)
+        super().__init__()
         self.rnn = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True,
                            bias=bias, bidirectional=bidirectional)
+    def forward(self, x):
+        x,_ = self.rnn(x)
+        return x
 
 
 class RNN2DBase(nn.Module):
@@ -188,12 +191,18 @@ class VanillaSequencerBlock(nn.Module):
         self.norm1 = norm_layer(dim)
         self.rnn_tokens = rnn_layer(dim, hidden_size, num_layers=num_layers, bidirectional=bidirectional)
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-        self.norm2 = norm_layer(dim)
-        self.mlp_channels = mlp_layer(dim, channels_dim, act_layer=act_layer, drop=drop)
+        self.norm2 = norm_layer(dim*2)
+        self.mlp_channels = mlp_layer(dim*2, channels_dim, act_layer=act_layer, drop=drop,out_features =512)
 
     def forward(self, x):
-        x = x + self.drop_path(self.rnn_tokens(self.norm1(x)))
-        x = x + self.drop_path(self.mlp_channels(self.norm2(x)))
+        x = self.norm1(x)
+        x = self.rnn_tokens(x)
+        x = x + self.drop_path(x)
+
+        x = self.norm2(x)
+        x = self.mlp_channels(x)
+        x = x + self.drop_path(x)
+        # x = x + self.drop_path(self.mlp_channels(self.norm2(x)))
         return x
 
 
@@ -224,6 +233,7 @@ class PatchEmbed(TimmPatchEmbed):
             x = x.flatten(2).transpose(1, 2)  # BCHW -> BNC
         else:
             x = x.permute(0, 2, 3, 1)  # BCHW -> BHWC
+            # x = x.permute(2,1,0)
         x = self.norm(x)
         return x
 
